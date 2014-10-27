5705590407c2cb495bcd2496f932d2457683d587
pockychen <chenhsuehyi@icloud.com>
2014-10-27 20:19:06 +0800

    initial

---
+++ application.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+
+$("#menu-button").click(function() {
+  if($(this).hasClass("menu-show")) {
+    $(this).removeClass("menu-show");
+    $(this).animate({ left: "+=300" }, 500);
+  }
+  else {
+    $(this).addClass("menu-show");
+    $(this).animate({ left: "-=300" }, 500);
+  }
+});
